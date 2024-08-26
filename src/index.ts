6533a034f2efba94aeb1e0c75e85703d1591259c
Ziad Khattab <ziadkh@amazon.com>
2024-08-26 01:22:25 -0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -15,19 +15,59 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { Resend } from "resend";
+import { Redis } from "@upstash/redis/cloudflare";
+export interface Env {
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+	RESEND_API_KEY: string;
+}
+
+const emails: string[] = [
+	"aegooby@gmail.com",
+];
+
 export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
-	async scheduled(event, env, ctx): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
-
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+	async scheduled(event, env: Env, ctx): Promise<void> {
+    const resend = new Resend(env.RESEND_API_KEY);
+		const redis = Redis.fromEnv(env);
+		const response = await fetch('https://slotted.co/graphql', {
+			headers: {
+				'content-type': 'application/json',
+			},
+			method: "POST",
+			body: JSON.stringify({
+				"query": "query { sheet(token:\"flowstatemic\") { events { date } title } }"
+			}),
+		});
+		if (response.ok) {
+			const body = await response.json();
+			console.log("Received response from Slotted GraphQL API:", JSON.stringify(body, undefined, 2));
+			const events = (body as any).data.sheet.events;
+			if (events.length > 0) {
+				const date = events[0].date;
+				const lastDate = await redis.get<string>("date");
+				if (date !== lastDate) {
+					console.log("Found new event sheet with date:", date);
+					const emailResult = await resend.emails.send({
+						from: "slotted@zeeyadkay.com",
+						to: emails,
+						subject: "Flow State Mic List Available",
+						html: '<p><a href="https://slotted.co/flowstatemic">Sign Up</a></p>'
+					});
+					if (emailResult.error) {
+						console.error("Email error:", emailResult.error.message);
+						throw new Error(emailResult.error.message);
+					}
+					console.log("Sent email with ID:", emailResult.data?.id);
+					await redis.set("date", date);
+				}
+			}
+		} else {
+			console.error("Slotted GraphQL API error:", response.statusText);
+			throw new Error(response.statusText);
+		}
 	},
 } satisfies ExportedHandler<Env>;
